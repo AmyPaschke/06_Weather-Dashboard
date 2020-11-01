08d91b876e23863bc45cf70db9f3971b0212f416
Amy Paschke <pasch049@umn.edu>
2020-11-01 14:25:45 -0600

    removed old functions and re-wrote ajax

--- assets/scriptold.js
+++ assets/scriptold.js
@@ -13,95 +13,71 @@ WHEN I open the weather dashboard
 THEN I am presented with the last searched city forecast */
 
 let APIKey = "31d362c3396b74ca1d3b07eb462756e2";
+let button = $("#button");
 let city = null;
 
-function queryCityWeather() {
-  city = $("#city-search").val();
+button.on("click", function(event) {
+    event.preventDefault();
+  city = $("#city-search").val()
   fetchResultsForCity(city);
-}
+  addToSearchHistory(city);
+  return city;
+})
 
 function renderCityWeather(results) {
   console.log(results);
 }
 
-function addToSearchHistory(city) {}
+let previousSearchArray = [];
+//this function adds the previously searched city name to local storage and then adds it under the search bar
+function addToSearchHistory(city) {
+    let previousCityName = localStorage.setItem("city-name", city);
+    previousCityName.city.push(previousSearchArray);
 
-/*async function fetchResultsForCity(city) {
-  var findCityResponse = await fetch(cityQueryUrl(city))
+    for (let m = 0; m < previousSearchArray.length; m++) {
+        let pElement = $("<p>");
+        pElement.addClass("previous-city-names");
+        pElement.text(previousSearchArray[i]);
+        $("#previous-city-search").prepend(pElement);
+        //let cityNamesLocalStorage = localStorage.getItem("city-name");
+        
+        
+        
+    }    
+}
+
+//this function will get the results from the user's search criteria
+async function fetchResultsForCity(city) {
+  let findCityResponse = await fetch(cityQueryUrl(city))
     .then(response => response.json());
 
   if (findCityResponse.cod != 200) {
-    console.log(findCityResponse);
     return displaySearchError(city, findCityResponse.message)
   }
 
-  var response = await fetch(weatherQueryUrl(findCityResponse.coord))
+  let response = await fetch(weatherQueryUrl(findCityResponse.coord))
     .then(response => response.json());
 
-  if (response.cod != undefined) {
-    console.log(response);
+  if (response.cod != 200) {
     return displaySearchError(city, response.message)
   } else {
     renderCityWeather(response);
   }
 
   return { error: "Cannot find city called " + city, result: []};
-}*/
-
-function fetchResultsForCity(city) {
-  let queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  })
-    .fail(function () {
-      displaySearchError(city, response.cod);
-    })
-    .done(function (response) {
-      let queryURL =
-        "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&lat=" +
-        response.coord.lat +
-        "&lon=" +
-        response.coord.lon +
-        "&appid=" +
-        APIKey;
-      $.ajax({
-        url: queryURL,
-        method: "GET",
-      })
-        .fail(function () {
-          displaySearchError(city, response.cod);
-        })
-        .done(function (response) {
-          renderCityWeather(response);
-        });
-    });
 }
 
+//this functions gives us the URL for the city information
 function cityQueryUrl(city) {
-  return (
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    APIKey
-  );
+  return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 }
 
+//this function gives us the URL for the weather information we need
 function weatherQueryUrl(coord) {
-  return (
-    "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&lat=" +
-    coord.lat +
-    "&lon=" +
-    coord.lon +
-    "&appid=" +
-    APIKey
-  );
+  return "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&lat=" + coord.lat + "&lon=" + coord.lon + "&appid=" + APIKey;
 }
 
 function displaySearchError(city, error) {
-  console.log(error + ' "' + city + '"');
+    console.log(error + " \"" + city + "\"");
 }
+
