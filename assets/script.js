97408bf229048b84ee46e5fd517bad58b6c28fa1
Amy Paschke <pasch049@umn.edu>
2020-11-01 12:04:20 -0600

    added additional functions to JS and changed search button on HTML

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,59 @@ THEN I am again presented with current and future conditions for that city
 WHEN I open the weather dashboard
 THEN I am presented with the last searched city forecast */
 
-let APIKey = "";
-let city = "userclicked option";
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + city + APIKey;
+let APIKey = "31d362c3396b74ca1d3b07eb462756e2";
+let button = $("#button");
+let city = null;
+
+button.on("click", function(event) {
+    event.preventDefault();
+  city = $("#city-search").val()
+  fetchResultsForCity(city);
+  addToSearchHistory(city);
+  return city;
+})
+
+function renderCityWeather(results) {
+  console.log(results);
+}
+
+//this function adds the previously searched city name to local storage and then adds it under the search bar
+function addToSearchHistory(city) {
+    let previousCityName = city;
+}
+
+//this function will get the results from the user's search criteria
+async function fetchResultsForCity(city) {
+  let findCityResponse = await fetch(cityQueryUrl(city))
+    .then(response => response.json());
+
+  if (findCityResponse.cod != 200) {
+    return displaySearchError(city, findCityResponse.message)
+  }
+
+  let response = await fetch(weatherQueryUrl(findCityResponse.coord))
+    .then(response => response.json());
+
+  if (response.cod != 200) {
+    return displaySearchError(city, response.message)
+  } else {
+    renderCityWeather(response);
+  }
+
+  return { error: "Cannot find city called " + city, result: []};
+}
+
+//this functions gives us the URL for the city information
+function cityQueryUrl(city) {
+  return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+}
+
+//this function gives us the URL for the weather information we need
+function weatherQueryUrl(coord) {
+  return "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&lat=" + coord.lat + "&lon=" + coord.lon + "&appid=" + APIKey;
+}
+
+function displaySearchError(city, error) {
+    console.log(error + " \"" + city + "\"");
+}
+
